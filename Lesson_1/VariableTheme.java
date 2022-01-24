3c86a118d996ac77e5452d6c0d440128d16027a3
ZaiPP <paul-zai@mail.ru>
2022-01-24 19:06:02 +0300

    implement HW_1

---
+++ Lesson_1/VariableTheme.java
@@ -0,0 +1,25 @@
+public class VariableTheme {
+    public static void main(String[] args) {
+
+        //TODO: Создание переменных и вывод их значений на консоль
+
+        byte yadro = 4;
+        short hz = 1700;
+        int ozu = 4096;
+        long disk = 473_828_421;
+        float speedlow = 1.754_685f;
+        double speedup= 2.454_685_645_825_463d;
+        char simbol = 'H';
+        boolean result = true;
+
+        System.out.println("Создание переменных и вывод их значений на консоль");
+        System.out.println(yadro);
+        System.out.println(hz);
+        System.out.println(ozu);
+        System.out.println(disk);
+        System.out.println(speedlow);
+        System.out.println(speedup);
+        System.out.println(simbol);
+        System.out.println(result);
+    }
+}
